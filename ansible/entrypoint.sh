4b80d7d142692038bf010bd8ec539188921abfd8
Igor Savrandeev <igorsavrandeev2002@gmail.com>
2024-05-28 20:04:50 +0300

    final editing container ansible

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-ulimit -n 65536
-
-echo "hacluster:123" | chpasswd
 
 service ssh start
-service nginx start
-service pacemaker start
-service corosync start
-service pcsd start
 
-# Запустить бесконечный цикл, чтобы контейнер не завершил выполнение
+# LB1_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' lb1)
+# LB2_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' lb2)
+
+# tail -f /dev/null
+
+# # Проверяем, что IP-адреса были получены
+# if [ -z "$LB1_IP" ] || [ -z "$LB2_IP" ]; then
+#   echo "Не удалось получить IP-адреса контейнеров lb1 и lb2"
+#   exit 1
+# fi
+
+# # Обновляем /etc/hosts
+# echo "$LB1_IP lb1" >> /etc/hosts
+# echo "$LB2_IP lb2" >> /etc/hosts
+
+# echo "Файл /etc/hosts успешно обновлен"
+
+
+
+ssh-keyscan lb1 >> /root/.ssh/known_hosts
+ssh-keyscan lb2 >> /root/.ssh/known_hosts
+
+ssh-keyscan backend1 >> /root/.ssh/known_hosts
+ssh-keyscan backend2 >> /root/.ssh/known_hosts
+
+ansible-playbook -i ansible/playbooks/inventory ansible/playbooks/playbook_main.yml
+
 tail -f /dev/null
